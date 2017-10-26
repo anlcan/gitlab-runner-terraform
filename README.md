3effab1d9420bf8408fc6c7d19024c8d3be8e685
anlcan <anil.canbaykal@finleap.com>
2017-10-26 19:22:33 +0200

    tfvars update & tokens

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
-Running gitlab-runner cmd:
+- Update `gitlab.tfvars`
+- Update tokens.txt with registration tokens for your projects found under `/settings/ci_cd`, one per line
+- Running gitlab-runner cmd:
+  terraform apply --var-file=gitlab.tfvars  
 
-
-    terraform {plan|apply|destroy} -var-file=`pwd`/secrets/finleap/secrets.tfvars  stacks/gitlab-runner 
-
-where 
-    - secrets.tfvars contains `access_key` and `secret_key`
-    - inventory-checker-credentials is aws credentials file for deployment
-    - tokens.txt is a text file with gitlab project id in each line
-    - ssh_keys.zip is a zip file containing all the <client>-<environment>-ssh.pem files
-    
-    terraform apply -var-file=`pwd`/secrets/finleap/secrets.tfvars  stacks/gitlab-runner 
--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ variable "secret_key" {
   default = ""
 }
 
+variable "gitlab-pem-path" {
+  default = ""
+  description = "pem file for aws"
+}
+
 
 provider "aws" {
   access_key = "${var.access_key}"
@@ -13,75 +18,15 @@ provider "aws" {
 }
 
 
-# Create an IAM role for the Web Servers.
-resource "aws_iam_role" "config_templates_bucket_role" {
-  name = "config_templates_bucket_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_iam_instance_profile" "config_template_role" {
   name = "config_template_role"
-  role = "${aws_iam_role.config_templates_bucket_role.name}"
 }
-
-resource "aws_iam_role_policy" "config_template_role_policy" {
-  name = "config_template_role_policy"
-  role = "${aws_iam_role.config_templates_bucket_role.id}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:HeadObject",
-        "s3:DeleteObject"
-      ],
-      "Resource": [
-      "arn:aws:s3:::configuration-templates/*",
-      "arn:aws:s3:::configuration-client-secrets/*"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-// bucket is created separetly
-//resource "aws_s3_bucket" "config_template_bucket" {
-//  bucket = "config_template_bucket"
-//  acl = "private"
-//  versioning {
-//    enabled = true
-//  }
-//  tags {
-//    Name = "bucket-name"
-//  }
-//}
 
 
 
 resource "aws_instance" "gitlab-runner" {
   ami           = "ami-060cde69"
   instance_type = "t2.medium"
-  key_name      = "finleap-dev"
   iam_instance_profile = "${aws_iam_instance_profile.config_template_role.id}"
   root_block_device {
     volume_size = 16
@@ -92,29 +37,20 @@ resource "aws_instance" "gitlab-runner" {
 
     connection {
       user = "ubuntu"
-      private_key = "${file("secrets/finleap/finleap-dev.pem")}"
+      private_key = "${file("gitlab-pem-path")}"
 
     }
   }
 
+  # tokens.txt should contain one registration token per line,
+  # found in /settings/ci_cd under your projects
   provisioner "file" {
-    source      = "stacks/gitlab-runner/build-script.sh"
-    destination = "/tmp/build-script.sh"
-
-    connection {
-      user = "ubuntu"
-      private_key = "${file("secrets/finleap/finleap-dev.pem")}"
-
-    }
-  }
-
-  provisioner "file" {
-    source      =  "secrets/finleap/tokens.txt"
+    source      =  "tokens.txt"
     destination = "/tmp/tokens"
 
     connection {
       user = "ubuntu"
-      private_key = "${file("secrets/finleap/finleap-dev.pem")}"
+      private_key = "${file("gitlab-pem-path")}"
 
     }
   }
@@ -125,13 +61,11 @@ resource "aws_instance" "gitlab-runner" {
       "sudo chmod +x /tmp/script.sh",
       "sudo /tmp/script.sh /tmp/tokens",
       "sudo rm /tmp/tokens",
-      "sudo cp /tmp/build-script.sh  /home/gitlab-runner/build-script.sh",
-      "sudo chown gitlab-runner /home/gitlab-runner/build-script.sh"
     ]
 
     connection {
       user = "ubuntu"
-      private_key = "${file("secrets/finleap/finleap-dev.pem")}"
+      private_key = "${file("gitlab-pem-path")}"
 
     }
   }
@@ -143,47 +77,7 @@ resource "aws_instance" "gitlab-runner" {
     connection {
 
       user = "ubuntu"
-      private_key = "${file("secrets/finleap/finleap-dev.pem")}"
-
-    }
-  }
-
-  # find . -maxdepth 3 -name '*-ssh.pem' | zip -j secrets/finleap/ssh_keys -@
-  provisioner "local-exec" {
-    command = "find . -maxdepth 3 -name '*-ssh.pem' | zip -j secrets/finleap/ssh_keys.zip -@"
-  }
-
-  provisioner "file" {
-    source = "secrets/finleap/ssh_keys.zip"
-    destination = "/home/ubuntu/ssh_keys.zip"
-
-    connection {
-      user = "ubuntu"
-      private_key = "${file("secrets/finleap/finleap-dev.pem")}"
-
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mkdir /home/gitlab-runner/.aws/",
-      "sudo chown gitlab-runner /home/gitlab-runner/.aws/",
-
-      "sudo mkdir /home/gitlab-runner/.ssh/",
-      "sudo chown gitlab-runner /home/gitlab-runner/.ssh/",
-
-      "sudo mv /home/ubuntu/credentials /home/gitlab-runner/.aws/credentials",
-      "sudo chown gitlab-runner  /home/gitlab-runner/.aws/credentials",
-      "sudo mv /home/ubuntu/ssh_keys.zip /home/gitlab-runner/.ssh/ssh_keys.zip",
-
-      "sudo unzip /home/gitlab-runner/.ssh/ssh_keys.zip -d /home/gitlab-runner/.ssh/",
-      "sudo chown gitlab-runner  /home/gitlab-runner/.ssh/*",
-      "sudo chmod 400 /home/gitlab-runner/.ssh/*"
-    ]
-
-    connection {
-      user = "ubuntu"
-      private_key = "${file("secrets/finleap/finleap-dev.pem")}"
+      private_key = "${file("gitlab-pem-path")}"
 
     }
   }
@@ -194,7 +88,7 @@ resource "aws_instance" "gitlab-runner" {
 
     connection {
       user = "ubuntu"
-      private_key = "${file("secrets/finleap/finleap-dev.pem")}"
+      private_key = "${file("gitlab-pem-path")}"
       host = "${aws_instance.gitlab-runner.public_ip}"
 
       }
@@ -203,8 +97,6 @@ resource "aws_instance" "gitlab-runner" {
   tags {
     Name = "terraform-loves-gitlab"
   }
-
-
 }
 
 
